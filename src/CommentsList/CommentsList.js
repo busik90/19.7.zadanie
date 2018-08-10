dc0755691fcfbad2e32f8db1d0c9c4a9550c1e88
Pawel Bus <pawelbus.dev@outlook.com>
2018-08-10 20:35:14 +0200

    Add 'addCommet' function

--- src/CommentsList/CommentsList.js
+++ src/CommentsList/CommentsList.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import Comment from '../Comment/CommentContainer';
 
-const CommentsList = ({ comments }) => <ul>{comments.map(comment => <Comment key={comment.id} {...comment} />)}</ul>;
+class CommentsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentText: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ commentText: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addComment(this.state.commentText);
+    this.setState({commentText: ''});
+  }
+
+  render() {
+    return (
+      <ul>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <textarea
+            name="comment"
+            cols="30"
+            rows="1"
+            onChange={e => this.handleChange(e)}
+            value={this.state.commentText}
+            placeholder='Comment message'
+          ></textarea>
+          <input type="submit" value='Dodaj komentarz'/>
+        </form>
+        {this.props.comments.map(comment => <Comment key={comment.id} {...comment} />)}
+      </ul>
+    )
+  }
+}
 
 export default CommentsList;
--- src/CommentsList/CommentsListContainer.js
+++ src/CommentsList/CommentsListContainer.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import CommentsList from '../CommentsList/CommentsList';
+import { comments } from '../actions';
 
 const mapStateToProps = state => ({
   comments: state.comments
 });
 
-export default connect(mapStateToProps)(CommentsList);
+const mapDispatchToProps = dispatch => ({
+  addComment: text => dispatch(comments.add(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentsList);
